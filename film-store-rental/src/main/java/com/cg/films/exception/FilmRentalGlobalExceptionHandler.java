bf3d71f7018736dff8813565839ca8f09e82ff8a
anshuman-raturi <anshuman.raturi@capgemini.com>
2025-11-06 17:16:05 +0530

    Add files via upload

--- film-store-rental/src/main/java/com/cg/films/exception/FilmRentalGlobalExceptionHandler.java
+++ film-store-rental/src/main/java/com/cg/films/exception/FilmRentalGlobalExceptionHandler.java
@@ -1,5 +1,16 @@
 package com.cg.films.exception;
-
+ 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+ 
+@RestControllerAdvice
+ 
 public class FilmRentalGlobalExceptionHandler {
-
+	@ExceptionHandler(value = MultipleActorsFoundException.class)
+	public ResponseEntity<String> multipleActorsFoundException(MultipleActorsFoundException ex){
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+ 
 }
