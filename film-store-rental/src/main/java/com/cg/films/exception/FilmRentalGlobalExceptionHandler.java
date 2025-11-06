f4a9ac3240c712b8994a802e1bbd778fb715a96a
mufimaimoon <mufaddal.maimoon@capgemini.com>
2025-11-06 22:18:16 +0530

    updated film exception file

--- film-store-rental/src/main/java/com/cg/films/exception/FilmRentalGlobalExceptionHandler.java
+++ film-store-rental/src/main/java/com/cg/films/exception/FilmRentalGlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.cg.films.exception;
  
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
  
 @RestControllerAdvice
  
@@ -12,5 +16,16 @@ public class FilmRentalGlobalExceptionHandler {
 	public ResponseEntity<String> multipleActorsFoundException(MultipleActorsFoundException ex){
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
 	}
- 
+	
+
+	@ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(Map.of("message", ex.getMessage()));
+    }
+
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleEntityNotFound(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", ex.getMessage()));
+    }
 }
