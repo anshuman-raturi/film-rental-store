efcb1e5604510f2112ed7f5c9a7281b08ac121d0
anshuman-raturi <anshuman.raturi@capgemini.com>
2025-11-06 09:17:19 +0530

    empty folder

--- src/main/java/com/cg/films/repository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cg.films.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cg.films.entity.Film;
-
-public interface FilmRepository extends JpaRepository<Film, Long> {
-}
--- src/main/java/com/cg/films/repository/LanguageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cg.films.repository;
- 
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-import com.cg.films.entity.Language;
- 
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-}
