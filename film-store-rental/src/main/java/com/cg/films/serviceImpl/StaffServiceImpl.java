bae91bd2dfb031df3334638331dd2aa4aa8c5ee5
Dusari-Navya <Dusari.Navya@Capgemini.com>
2025-11-06 15:57:51 +0530

    Add files via upload

--- film-store-rental/src/main/java/com/cg/films/serviceImpl/StaffServiceImpl.java
+++ film-store-rental/src/main/java/com/cg/films/serviceImpl/StaffServiceImpl.java
@@ -1,5 +1,33 @@
 package com.cg.films.serviceImpl;
+ 
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cg.films.entity.Staff;
+import com.cg.films.entity.Store;
+import com.cg.films.repository.StaffRepository;
+import com.cg.films.repository.StoreRepository;
+import com.cg.films.service.StaffService;
+ 
+@Service
+public class StaffServiceImpl implements StaffService {
+ 
+    @Autowired
+    private StaffRepository staffRepository;
 
-public class StaffServiceImpl {
 
+ 
+    @Autowired
+    private StoreRepository storeRepository;
+ 
+ 
+  @Override
+  public List<Staff> getStaffByManagerId(Long managerId) {
+      Store store = storeRepository.findByManager_StaffId(managerId);
+      return (store != null) ? staffRepository.findByStore_StoreId(store.getStoreId()) : List.of();
+  }
+ 
+ 
+   
 }
--- film-store-rental/src/main/java/com/cg/films/serviceImpl/StoreServiceImpl.java
+++ film-store-rental/src/main/java/com/cg/films/serviceImpl/StoreServiceImpl.java
@@ -1,5 +1,29 @@
 package com.cg.films.serviceImpl;
 
-public class StoreServiceImpl {
-
+import java.util.List;
+import java.util.stream.Collectors;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+ 
+import com.cg.films.repository.StoreRepository;
+import com.cg.films.service.StoreService;
+ 
+@Service
+public class StoreServiceImpl implements StoreService {
+ 
+    @Autowired
+    private StoreRepository storeRepository;
+ 
+    
+ 
+    @Override
+       public List<Long> getAllManagerIds() {
+           return storeRepository.findAll()
+                                 .stream()
+                                 .map(store -> store.getManager().getStaffId())
+                                 .collect(Collectors.toList());
+       }
+ 
+    
 }
